fa9806d7b0d624895f088825a3c47eb8b868b8b2
yamagovi <code.artigo@gmail.com>
2024-09-26 14:19:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ app.use(express.json());
 app.use(cors({origin:'*'})) // el asterisco es para que se conecte a todas las rutas que hemos creado de manera remota en el servidor de mongodb
 app.use('/api/usuarios',require('./rutas/usuario.rutas'));
 app.use('/api/empleados',require('./rutas/empleado.rutas'));
-app.listen(3005, ()=> {
-    console.log("El servidor esta en el puerto 3005")
-})
+//app.listen(3005, ()=> {
+//    console.log("El servidor esta en el puerto 3005")
+//})
+//reemplazo el comentario de arriba por:
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`El servidor está corriendo en el puerto ${process.env.PORT || 3000}`);
+});
+
 
 dbconnect();
